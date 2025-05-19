d64752be4f4f3fc06e9cf866573b83a3df34d5d0
SatakeIsao <145173938+SatakeIsao@users.noreply.github.com>
2025-05-19 16:51:21 +0900

    プロジェクト、エンジンの追加 (#1)
    * GameTemplate・READMEの追加
    
    * エンジンの追加

---
+++ Game/GameCamera.cpp
@@ -0,0 +1,95 @@
+#include "stdafx.h"
+#include "GameCamera.h"
+
+GameCamera::GameCamera()
+{
+}
+
+GameCamera::~GameCamera()
+{
+}
+
+bool GameCamera::Start()
+{
+	//注視点から視点までのベクトルを設定
+	m_toCameraPos.Set(0.0f, 70.0f, -200.0f);
+	//m_toCameraPos *= 0.5f;
+	
+	//カメラのニアクリップとファークリップを設定
+	g_camera3D->SetNear(1.0f);
+	g_camera3D->SetFar(80000.0f);
+
+	//ばねカメラの初期化
+	m_springCamera.Init(
+		*g_camera3D,	//ばねカメラの処理を行うカメラを指定する
+		1000.0f,		//カメラの移動速度の最大値
+		true,			//カメラと地形とのあたり判定を取るかどうかのフラグ。trueだとあたり判定を行う
+		5.0f			//カメラに設定される球体コリジョンの半径。第3引数がtrueの時に有効になる
+	);
+	return true;
+}
+
+void GameCamera::Update()
+{
+	// プレイヤーの位置と回転を取得
+	//Vector3 target = m_player->GetPostion(); // プレイヤーの現在位置
+	//Quaternion playerRotation = m_player->GetRotation(); //プレイヤーの回転
+
+	//if (m_player->GetIsPathMoving())
+	//{
+	//	// **パス移動中はカメラの位置をプレイヤーの相対位置に固定するが、追従する**
+	//	if (!m_isCameraFixed)
+	//	{
+	//		// **カメラの相対位置を記録**
+	//		m_fixedCameraOffset = g_camera3D->GetPosition() - target;
+	//		m_isCameraFixed = true;
+	//	}
+
+	//	// **プレイヤーの位置が動いたらカメラもその分動かす**
+	//	Vector3 cameraPosition = target + m_fixedCameraOffset;
+	//	Vector3 cameraTarget = target + Vector3(0.0f, 70.0f, 0.0f); // 注視点はプレイヤーの少し上
+
+	//	m_springCamera.SetPosition(cameraPosition);
+	//	m_springCamera.SetTarget(cameraTarget);
+	//}
+	//else
+	//{
+	//	// **通常時はプレイヤーの背後を追従**
+	//	m_isCameraFixed = false; // 固定解除
+
+	//	Vector3 offset(m_offsetPos); // 通常時のカメラの位置
+	//	playerRotation.Apply(offset); // プレイヤーの回転を適用
+
+	//	Vector3 cameraPosition = target + offset;
+	//	Vector3 cameraTarget = target;
+	//	cameraTarget.y += 70.0f;
+
+	//	m_springCamera.SetPosition(cameraPosition);
+	//	m_springCamera.SetTarget(cameraTarget);
+	//}
+
+	m_springCamera.Update();
+
+
+	//// 右スティックの入力でカメラを左右に回転させる
+	//m_stickPowerX = g_pad[0]->GetRStickXF(); // X軸のスティック入力
+	//// プレイヤーの周りをY軸回転でカメラを移動
+	//Quaternion yRotation;
+	//yRotation.SetRotationDeg(Vector3::AxisY, 2.4f * m_stickPowerX); // Y軸周りに回転
+	//yRotation.Apply(m_toCameraPos); // カメラのオフセットに回転を適用
+
+	//プレイヤーの向きをカメラが向いている方向にする
+	//カメラの正面ベクトルを取得する
+	//Vector3 forward = g_camera3D->GetForward();
+	//プレイヤーが斜めを向かないようにyをゼロにする
+	//forward.y = 0.0f;
+	//正規化
+	//forward.Normalize();
+
+	////用意した情報でプレイヤーのローテーションを用意する
+	//Quaternion playerRotation;
+	////正面ベクトル(0,0,1)からカメラの正面ベクトルに向かう回転を作る
+	//playerRotation.SetRotation(Vector3::AxisZ, forward);
+	////プレイヤーのローテーションに適用する
+	//m_player->SetRotation(playerRotation);
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# stk_shooting
+シューティングゲーム
+・命名規則
+　名前空間→小文字
+　例：namespace app →アプリケーション側に絶対つける
+			エンジンにはつけない
+　アプリケーションの中に、名前空間を入れていく。
+　例：namespace actor →　役者という意味見た目がついているものすべて該当
+
+　クラス（関数）構造体名→UpperCamelCase(アッパーキャメルケース）
+　
+　変数→camelCase
+　　1文字目だけ小文字
+
+　定数→UpperSnakeCase
+    全部大文字、単語区切りアンダーバー（_）
+
+　メンバ変数→private、protectedの場合：最後にアンダーバー
+			例：camelCase_
+
+		publicの場合：そのまま,アンダーバーなし
+			例：camelCase
+
+ファイル構成
+　・Actor→見た目が存在するオブジェクトすべて継承
+　　・Character→キャラクターとなるもの
+　　　・BattleCharacter→バトル中に存在するキャラクター例；Playerとか
+　　　・EnemyCharacter→バトル中に存在する敵
+
+	
+　・CharacterSteering→キャラ操作するためのクラス、操作対象のバトルキャラクターのポインタを参照する
+
+　・StateMachine→ステートマシン、各キャラが持つステートを操作する
+　　・CharacterStateMachine
+　　　・BattleCharacterStateMachine
+
+
+文字化け
+visualStudio
+エクスプローラー
